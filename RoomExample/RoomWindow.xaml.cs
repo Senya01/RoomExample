e0e4d9affc9b37f9f7b4f82e1a432280ae841607
Senya <arseniy-selin-2004@yandex.ru>
2023-02-01 17:27:55 +0300

    Add files via upload

--- RoomExample/RoomWindow.xaml.cs
+++ RoomExample/RoomWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using RoomLibrary;
 
 namespace RoomExample
 {
@@ -19,9 +20,41 @@ namespace RoomExample
     /// </summary>
     public partial class RoomWindow : Window
     {
+        Room room = new Room();
+        LivingRoom livingRoom = new LivingRoom();
+        Office office = new Office();
+
         public RoomWindow()
         {
             InitializeComponent();
+        }
+
+        private void BAddRoom_Click(object sender, RoutedEventArgs e)
+        {
+            room.RoomLength = Convert.ToDouble(TBLengthR.Text);
+            room.RoomWidth = Convert.ToDouble(TBWidthR.Text);
+        }
+
+        private void BAddLivingRoom_Click(object sender, RoutedEventArgs e)
+        {
+            livingRoom.RoomLength = Convert.ToDouble(TBLengthL.Text);
+            livingRoom.RoomWidth = Convert.ToDouble(TBWidthL.Text);
+            livingRoom.NumWin = Convert.ToInt32(TBNumW.Text);
+        }
+
+        private void BAddOffice_Click(object sender, RoutedEventArgs e)
+        {
+            office.RoomLength = Convert.ToDouble(TBLengthO.Text);
+            office.RoomWidth = Convert.ToDouble(TBWidthO.Text);
+            office.NumSockets = Convert.ToInt32(TBNumS.Text);
+        }
+
+        private void BGetList_Click(object sender, RoutedEventArgs e)
+        {
+            ListRooms.Content = "";
+            ListRooms.Content += room.Info() + "\n";
+            ListRooms.Content += livingRoom.Info() + "\n";
+            ListRooms.Content += office.Info() + "\n";
         }
     }
 }
